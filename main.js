663b94bce9c3bda90d6bc2d89244bb3ee9d2c15a
Jose Rodriguez <47047097+jarodri01@users.noreply.github.com>
2020-05-03 11:46:59 -0500

    create Javascript file

---
+++ main.js
@@ -0,0 +1,65 @@
+(function () {
+    function giphySearchEngine(keyword, limit) {
+      return fetch(`http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=h5nEbFf0nz3FmKUhFMFajDQpM6ew66XF&limit=${limit}`)
+        .then(response => response.json());
+    }
+//h5nEbFf0nz3FmKUhFMFajDQpM6ew66XF// apikey 
+//https://api.giphy.com/v1/gifs/search?api_key=h5nEbFf0nz3FmKUhFMFajDQpM6ew66XF&q=&limit=18&offset=0&rating=G&lang=en
+  
+    function images (img) {
+      let $div = $('<div class="row"></div>');
+      $('<div class="col"></div>').append(img).appendTo($div);
+      $('#show').append($div)
+    }
+  
+    function resultLoad(img) {
+      return new Promise((resolve, reject) => {
+        img.onload = resolve;
+      });
+    }
+  //search field text box 
+    (function listenOnFormSubmit() {
+      $('#searchForm').submit(async (ev) => {
+        ev.preventDefault();
+  
+        let $input = $('#searchInput');
+  
+        main($input.val());
+      });
+       
+     
+    })
+    ();
+    async function main(keyword) {
+      //async function search(keyword){
+       
+      const result = await giphySearchEngine(keyword);
+      $('#show').html('');    
+      
+      let promises = [];
+      result.data.forEach(gif => {
+         let img = new Image();
+         img.src = gif.images.downsized.url;
+         promises.push(resultLoad(img));
+        images (img);
+      });
+    }
+  
+       //Gif limit buttons
+    //  $('#btn6').on('click', async function() {
+    //     console.log('Get me 6 results!');
+    //     const newResults = await giphySearchEngine(keyword, 6);
+    //   });
+    
+    //   $('#btn12').on('click', async function() {
+    //     console.log('Get me 12 results!');
+    //     const newResults = await giphySearchEngine(keyword, 12);
+    //   });
+    
+    //   $('#btn18').on('click', async function() {
+    //     console.log('Get me 18 results!');
+    //     const newResults = await giphySearchEngine(keyword, 18);
+    //   });
+    // }
+  })();
+
